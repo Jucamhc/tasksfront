72871b3be99da84a7f52917a25e385007ec5817b
Jucamhc <camilo2251@gmail.com>
2023-08-14 13:24:42 -0500

    finish crud

--- src/context/useTasks.tsx
+++ src/context/useTasks.tsx
@@ -3,7 +3,6 @@ import { TaskContext } from "./TaskContext";
 
 export const useTasks = () => {
   const context = useContext(TaskContext);
-
   if (!context) throw new Error("useTasks must be used within a TaskProvider");
   return context;
 };
